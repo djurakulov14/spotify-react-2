8fea4e4e861597bfba2a6153f1f7f291b23d815e
Sardor Djurakulov <sarik.j2445@gmail.com>
2024-08-21 15:40:30 +0500

    added some

--- src/Contexts/spotifyContext.js
+++ src/Contexts/spotifyContext.js
@@ -3,7 +3,7 @@ import { createContext } from "react";
 
 const spotify = createContext({
     CLIENT_ID: '9366a093ae6045079013cd025ea3ead5',
-    REDIRECT_URI: 'https://spotifyyy-cloneee.netlify.app/',
+    REDIRECT_URI: 'http://localhost:5173/',
     AUTH_ENDPOINT: 'https://accounts.spotify.com/authorize',
     RESPONSE_TYPE: 'token'
 })
