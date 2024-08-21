6d4b3593cec78fa531ebe66b95925f3aa2e05e1f
Sardor Djurakulov <sarik.j2445@gmail.com>
2024-08-21 15:29:35 +0500

    added some changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ import { IoMdArrowDropup } from "react-icons/io";
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = ({user}) => {
     
@@ -64,7 +64,7 @@ const Header = ({user}) => {
                 'aria-labelledby': 'basic-button',
                 }}
             >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}><Link to={'/profile'}>Profile</Link></MenuItem>
                 <MenuItem onClick={handleClose}>My account</MenuItem>
                 <MenuItem onClick={localStorageClear}>Logout</MenuItem>
             </Menu>
