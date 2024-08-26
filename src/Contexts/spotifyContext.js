9535d64923b93ffbd614a60ab8be99c4c014f326
Muhammadrizo <muhammadrizo281@gmail.com>
2024-08-26 06:00:59 +0500

    playlist_adaptive_50%

--- src/Contexts/spotifyContext.js
+++ src/Contexts/spotifyContext.js
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 
 const spotify = createContext({
-    CLIENT_ID: '9366a093ae6045079013cd025ea3ead5',
+    CLIENT_ID: 'f5b49fe53f204f9890362b3539064ad2',
     REDIRECT_URI: 'http://localhost:5173/',
     AUTH_ENDPOINT: 'https://accounts.spotify.com/authorize',
     RESPONSE_TYPE: 'token'
